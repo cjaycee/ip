f368985c69fc19d6c47d0dcdc46229ae0ffb3bf0
Jaycee <jiacheng2003@gmail.com>
2025-08-22 00:25:30 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Usagi.java
@@ -0,0 +1,11 @@
+public class Usagi {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                        "Hello! I'm Usagi\n" +
+                        "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________"
+        );
+    }
+}
