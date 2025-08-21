07d9ebf42b2fada3c4df33dfabae3b4f7baa76f0
Jaycee <jiacheng2003@gmail.com>
2025-08-22 00:37:57 +0800

    Echo

--- src/main/java/Usagi.java
+++ src/main/java/Usagi.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Usagi {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                        "Hello! I'm Usagi\n" +
-                        "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________"
-        );
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Usagi");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            System.out.println(input);
+
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+        scanner.close();
     }
 }
