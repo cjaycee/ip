8f057c2aeb174852c06061e4081e6fa0b6ee98c9
Jaycee <jiacheng2003@gmail.com>
2025-08-30 23:58:48 +0800

    Implement Storage class

--- src/main/java/Usagi.java
+++ src/main/java/Usagi.java
@@ -41,11 +41,12 @@ public class Usagi {
     public static void main(String[] args) {
         TaskList tasks = new TaskList();
         Ui ui = new Ui();
+        Storage storage = new Storage("data/Usagi.txt");
 
         ui.greet();
 
         try {
-            tasks = readFileContent("data/Usagi.txt");
+            tasks = storage.readFileContent();
         } catch (IOException e) {
             System.out.println("Something went wrong while reading the file: " + e.getMessage());
         }
@@ -62,72 +63,10 @@ public class Usagi {
         ui.closeScanner();
 
         try {
-            writeFileContent(tasks, "data/Usagi.txt");
+            storage.writeFileContent(tasks);
         } catch (IOException e) {
             System.out.println("Something went wrong while reading the file: " + e.getMessage());
         }
-    }
-
-    private static TaskList readFileContent(String filePath) throws IOException {
-        TaskList tasks = new TaskList();
-
-        File f = new File(filePath);
-        File folder = f.getParentFile();
-
-        if (!folder.exists()) {
-            folder.mkdirs();
-        }
-
-        if (!f.exists()) {
-            f.createNewFile();
-            return tasks; // empty list on first run
-        }
-
-        Scanner s = new Scanner(f);
-        while (s.hasNextLine()) {
-            String line = s.nextLine().trim();
-            if (line.isEmpty()) continue;
-
-            String[] parts = line.split("\\|");
-            for (int i = 0; i < parts.length; i++) {
-                parts[i] = parts[i].trim();
-            }
-
-            String type = parts[0]; // T, D, or E
-            boolean isDone = parts[1].equals("1");
-            String desc = parts[2];
-
-            Task t = null;
-
-            switch (type) {
-            case "T":
-                t = new Todo(desc);
-                break;
-            case "D":
-                t = new Deadline(desc, parts[3]);
-                break;
-            case "E":
-                t = new Event(desc, parts[3], parts[4]);
-                break;
-            }
-
-            if (isDone && t != null) {
-                t.markAsDone();
-            }
-            if (t != null) {
-                tasks.add(t);
-            }
-        }
-        s.close();
-        return tasks;
-    }
-
-    private static void writeFileContent(TaskList tasks, String filePath) throws IOException {
-        FileWriter fw = new FileWriter(filePath);
-        for (int i = 0; i < tasks.size(); i++) {
-            fw.write(tasks.get(i).toFileString() + System.lineSeparator());
-        }
-        fw.close();
     }
 
 }
